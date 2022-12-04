720aa94d85122f1e87d271361d477d9181f2c995
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-04 22:08:49 +0530

    keydown

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ outer.addEventListener('mouseout', function(){
 outerDiv.addEventListener('mouseenter', function() {
     console.log('Mouse enter ');
 });
+
+var search = document.getElementById('search');
+
+search.addEventListener('keypress', function() {
+    console.log('this event is key pressed');
+});
+
+search.addEventListener('keydown', function() {
+    console.log("this event is key down");
+});
+
+search.addEventListener('keyup', function() {
+    console.log('this is a key up event');
+})
