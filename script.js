e80f6f191ac5d68c15b6468c1da6dfe1cac6aeee
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-04 21:58:12 +0530

    mouse

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ var outerDiv = document.getElementById('outer');
 
 outerDiv.addEventListener('mouseover', function(){
     console.log('Mouse over pointer');
-})
+});
+
+outer.addEventListener('mouseout', function(){
+    console.log('Mouse out ');
+});
